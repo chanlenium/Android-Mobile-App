7050c5e530eb291edd57f4af4530288f4923ab27
DanielOh <boxone80@gmail.com>
2021-01-22 15:37:57 -0500

    hide keyboard when user press the submit button

--- 02_Views and Activities/MyWorkshop01/app/src/main/java/com/example/myworkshop01/MainActivity.java
+++ 02_Views and Activities/MyWorkshop01/app/src/main/java/com/example/myworkshop01/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.myworkshop01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -37,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         submitBtn = findViewById(R.id.submitBtn);
         submitBtn.setOnClickListener(new View.OnClickListener() {// To specify an action when the button is pressed, set a click listener on the button object
             public void onClick(View v) {
+                hideKeyboard();
+
                 // display user Info when the user input is validated
                 if (validateInput(fisrtNameEt.getText().toString(), lastNameEt.getText().toString())) {
                     userInfoTv.setText(getFormattedMessage(fisrtNameEt.getText().toString(), lastNameEt.getText().toString()));
@@ -58,6 +62,17 @@ public class MainActivity extends AppCompatActivity {
         messageBuilder.append(lName);
 
         return messageBuilder.toString();
+    }
+
+    public void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(this);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     private boolean validateInput(String fName, String lName) {

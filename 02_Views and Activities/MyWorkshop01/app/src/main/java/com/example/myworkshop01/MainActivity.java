db9c80c24320fa1742655152d47c83d5c3524c80
Dongchan Oh <dynamic.oh@gmail.com>
2021-01-23 21:34:10 -0500

    code update
    code update

--- 02_Views and Activities/MyWorkshop01/app/src/main/java/com/example/myworkshop01/MainActivity.java
+++ 02_Views and Activities/MyWorkshop01/app/src/main/java/com/example/myworkshop01/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     // variable initialization
     private TextView userInfoTv;
-    private EditText fisrtNameEt, lastNameEt;
+    private EditText firstNameEt, lastNameEt;
     private Button submitBtn;
 
     @Override
@@ -25,28 +25,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // Refer to Layout
 
-        initUI();
+        initUI();   // Call initialization function
     }
 
     // UI View initialization
     private void initUI() {
         userInfoTv = findViewById(R.id.userInfoTv);
-        userInfoTv.setText(getString(R.string.hi_1));
+        userInfoTv.setText(getString(R.string.hi_1));   // set string value("Hi!") defined in strings.xml as initial value
 
-        fisrtNameEt = findViewById(R.id.fisrtNameEt);
+        firstNameEt = findViewById(R.id.firstNameEt);
         lastNameEt = findViewById(R.id.lastNameEt);
 
         submitBtn = findViewById(R.id.submitBtn);
         submitBtn.setOnClickListener(new View.OnClickListener() {// To specify an action when the button is pressed, set a click listener on the button object
             public void onClick(View v) {
-                hideKeyboard();
+                hideKeyboard(); // call the function to hide a keyboard
 
-                // display user Info when the user input is validated
-                if (validateInput(fisrtNameEt.getText().toString(), lastNameEt.getText().toString())) {
-                    userInfoTv.setText(getFormattedMessage(fisrtNameEt.getText().toString(), lastNameEt.getText().toString()));
+                // display user information when the user input is validated
+                if (validateInput(firstNameEt.getText().toString(), lastNameEt.getText().toString())) { // pass the value as primitive type
+                    userInfoTv.setText(getFormattedMessage(firstNameEt.getText().toString(), lastNameEt.getText().toString()));
 
                     // print output in Logcat window (debug message)
-                    Log.d("USER_INFO", "First name is "+ fisrtNameEt.getText().toString() + ", Last Name is " + lastNameEt.getText().toString());
+                    Log.d("USER_INFO", "First name is "+ firstNameEt.getText().toString() + ", Last Name is " + lastNameEt.getText().toString());
                 }else{
                     userInfoTv.setText(getString(R.string.hi_there));
                 }
@@ -54,11 +54,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // Gets the Message formatted as a String.
+    // Each Message implementation determines the appropriate way to format the data encapsulated in the Message.
+    // Messages that provide more than one way of formatting the Message will implement MultiformatMessage.
     private String getFormattedMessage(String fName, String lName) {
         return String.format(getString(R.string.hi_message_format), fName, lName);
     }
 
-    public void hideKeyboard() {
+    public void hideKeyboard() {    // keyboard is hidden once clicking a button
         InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
         //Find the currently focused view, so we can grab the correct window token from it.
         View view = getCurrentFocus();

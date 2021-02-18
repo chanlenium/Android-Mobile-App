7e854025d85f7df14d48fa23ce890988ade90e91
Dongchan Oh <dynamic.oh@gmail.com>
2021-02-17 19:53:42 -0500

    update optionmenu

--- 05_Introduction to Recycler View/MenuExample/app/src/main/java/com/example/menuexample/MainActivity.java
+++ 05_Introduction to Recycler View/MenuExample/app/src/main/java/com/example/menuexample/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /** Creating an Options Menu **/
+    // To specify the options menu for an activity,
+    // override onCreateOptionsMenu() (fragments provide their own onCreateOptionsMenu() callback).
+    // In this method, you can inflate your menu resource(defined in XML) into the Menu provided in the callback.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -24,16 +28,25 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /** Handling click events **/
+    // When the user selects an item from the options menu,
+    // the system calls your activity's onOptionsItemSelected() method.
+    // This method passes the MenuItem selected.
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.item1){
-            Toast.makeText(this, " you clicked item 1", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, this.getString(R.string.toastMsg) + " "
+                    + this.getString(R.string.item1), Toast.LENGTH_SHORT).show();
             return true;
         }else if(id == R.id.item2){
-
+            Toast.makeText(this, this.getString(R.string.toastMsg) + " "
+                    + this.getString(R.string.item2), Toast.LENGTH_SHORT).show();
+            return true;
         }else if(id == R.id.item3){
-
+            Toast.makeText(this, this.getString(R.string.toastMsg) + " "
+                    + this.getString(R.string.item3), Toast.LENGTH_SHORT).show();
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
